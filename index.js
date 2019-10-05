0efee3454629d3bb7738e34ce69af16001e98f34
tfrikker <tom.frikker@gmail.com>
2019-10-05 18:06:47 -0400

    Added bare-bones framework for receiving new trades

--- index.js
+++ index.js
@@ -17,4 +17,84 @@ app.get('/', function(request, response) {
 	response.sendFile(__dirname + '/public/index.html');
 });
 
+// generate random int between min (inclusive) and max (exclusive)
+function randIntRange(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomBuyer() {
+	var randomBuyerIndex = randIntRange(0, buyers.length);
+	console.log("index: " + randomBuyerIndex);
+	return buyers[randomBuyerIndex];
+}
+
+function generateTrade(buyer, inventory) {
+	//TODO implement
+	return {
+		"itemsSelling": [items["moldyOrange"], items["sodaCan"]],
+		"itemsBuying": [items["egg"]]
+	}
+}
+
+function getItem(id) {
+	items.forEach(function(element) {
+  		if (element.id == id) {
+			return element;
+		}
+	);
+}
+
+app.get('/newTrade', function(request, response) {
+	var inventory = request.body.inventory;
+	var buyer = generateRandomBuyer();
+	console.log("HEY");
+	var trade = generateTrade(buyer, inventory);
+	response.send({
+		"buyer": buyer,
+		"trade": trade
+	});
+});
+
 app.listen(process.env.PORT || 5000);
+
+var items = [
+	{
+		id: "moldyOrange",
+		name: "Moldy Orange",
+		tags: ["food", "plant", "spoiled"],
+		image: "/moldy_orange.png",
+		text: "Gross moldy fruit. Blech"
+	},
+	{
+		id: "egg",
+		name: "Egg",
+		tags: ["food"],
+		image: "/egg.png",
+		text: "It's hardboiled"
+	},
+	{
+		id: "sodaCan",
+		name: "Soda Can",
+		tags: ["food", "junk"],
+		image: "/soda_can.png",
+		text: "Something liquid is inside"
+	}
+]
+
+
+var buyers = [
+	{
+		id: "plagueRat",
+		name: "Plague Doctor Rat",
+		image: "public/test_image.tif",
+		typePrefs: {
+			"food": 10,
+			"junk": 2
+		},
+		itemPrefs: {
+			"moldyDonut": 15
+		},
+		dealSize: 20,
+		saviness: 1
+	}
+];
