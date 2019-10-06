1e9888b08336ad1157c0318de2fb5a56411a9094
Samtfm <sfmemail@gmail.com>
2019-10-06 13:05:44 -0700

    small portrait update

--- src/trade_ui.js
+++ src/trade_ui.js
@@ -1,7 +1,7 @@
 import { processTrade } from './gameState';
 import { getTexture } from './texture_bag';
 import { WIDTH, HEIGHT } from './index';
-import { InspectableImage } from './base_ui';
+import { InspectableImage, BGElem } from './base_ui';
 import { MESSAGE_STYLE } from './styles';
 
 
@@ -13,18 +13,15 @@ export class TradeWindow{
         const portraitHeight = 128;
 
         this.container = new PIXI.Container();
-        this.background = new PIXI.Graphics();
 
-        this.background.beginFill(0xffffff);
-        this.background.drawRect(0, 0, WIDTH, HEIGHT);
-        this.background.endFill();
+
+        this.background = BGElem(0, 0, WIDTH, HEIGHT, 0xFFFFFF);
         this.container.addChild(this.background);
 
         this.portrait = new InspectableImage({image: './img/test_image.png', text:'flavor!'})
         this.portrait.width = this.portrait.width/this.portrait.height*portraitHeight
         this.portrait.height = portraitHeight
         this.container.addChild(this.portrait.sprite);
-
 
         this.offerings = new PIXI.Container();
 
